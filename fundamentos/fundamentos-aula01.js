093e788e079c3f0421b7419b8b6d8da564e3592e
Dev-Kauan <kauanlima127@gmail.com>
2024-03-01 21:52:05 -0300

    Aula do dia 01/03

---
+++ fundamentos/fundamentos-aula01.js
@@ -0,0 +1,10 @@
+// Este código é um comentário de uma linha
+
+/*
+Comentar mais de uma linha
+*/
+
+var nome = 'Kauan';
+
+// Imprimi alguma coisa no console
+console.log("Olá", nome);
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,17 @@
+// Criando variáveis com var
+var nomeCompleto
+
+// Atribuição na variável
+nomeCompleto = "Kauan Cleiton";
+
+var primeiroNome = 'Kauan';
+var nomeDoMeio = 'Mathias';
+var ultimoNome = 'Souza';
+
+console.log(`Nome: ${primeiroNome} ${nomeDoMeio} ${ultimoNome}`);
+
+//Criando variáveis com o const
+const idade = 25;
+
+//Criando variáveis com o let
+let primeiroNome = 'Kauan';
