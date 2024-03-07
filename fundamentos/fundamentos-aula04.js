70e14651e84e6448200a2f6ffabcc9b03cfcb1b5
Dev-Kauan <kauanlima127@gmail.com>
2024-03-07 20:32:42 -0300

    Revisão do JavaScript

---
+++ fundamentos/fundamentos-aula04.js
@@ -0,0 +1,20 @@
+// Estrutra de condição
+// if else
+
+let nomeAluno = 'Kauan';
+let idade = 18;
+let curso = 'ADS';
+
+if (idade >= 18) {
+    console.log(`${nomeAluno}, você é maior de idade`);
+} else {
+    console.log(`${nomeAluno}, você é menor de idade`)
+}
+
+idade >= 18 && curso == "ADS" ? console.log("Aluno Válido") : console.log("Aluno Inválido");
+
+// Estrutura de repetição
+// for
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
