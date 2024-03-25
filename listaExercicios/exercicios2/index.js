ffb9a304bb8a903b13b2221790d25754000ae658
Dev-Souza <kauan.souza.dev@gmail.com>
2024-03-25 16:45:39 -0300

    Lista 2 exercicio até o número 7 resolvido.

--- listaExercicios/exercicios2/index.js
+++ listaExercicios/exercicios2/index.js
@@ -48,7 +48,7 @@ app.post('/ex04/', (req, res) => {
     const distancia = Number(req.body.distancia);
     const tempo = Number(req.body.tempo);
 
-    res.send(`A velocidade média do ${nome}, foi ${distancia/tempo} km/h.`)
+    res.send(`A velocidade média do ${nome}, foi ${distancia / tempo} km/h.`)
 });
 
 //5. Faça uma api que calcule e imprima o salário reajustado de um funcionário de acordo com a seguinte regra:
@@ -59,7 +59,7 @@ app.post('/ex05/', (req, res) => {
     const nome = req.body.nome;
     let salario = Number(req.body.salario);
 
-    if(salario <= 2000){
+    if (salario <= 2000) {
         let reajuste = salario * 0.50;
         return res.send(`${nome}, seu salário atual é: R$${salario}.
         Com o reajuste seu salário foi para: R$${salario + reajuste}`);
@@ -68,6 +68,38 @@ app.post('/ex05/', (req, res) => {
     res.send(`${nome}, seu salário atual é: R$${salario}.
     Com o reajuste seu salário foi para: R$${salario + reajuste}`)
 });
+
+//6. Construa uma api que calcule o peso ideal de uma pessoa. Dados de entrada: altura e sexo. Fórmulas para cálculo do peso:
+//• peso ideal de homem = (72,7 x altura) – 58
+//• peso ideal da mulher = (62,1 x altura) – 44,7
+
+app.post('/ex06/', (req, res) => {
+    const altura = Number(req.query.altura);
+    const sexo = req.query.sexo;
+    
+    let imc = (72.7 * altura) - 58;
+    if (sexo == 'Masculino') return res.send(`O peso ideal para homem com a altura informada é: ${imc.toFixed(2)} kilos`);
+    
+    imc = (62.1 * altura) - 44.7;
+    if (sexo == 'Feminino') return res.send(`O peso ideal para mulher com a altura informada é: ${imc.toFixed(2)}`);
+});
+
+//7. Faça uma api para ler o código e o preço de 15 produtos, calcular e escrever:
+//• O maior preço lido; e
+//• A média aritmética dos preços dos produtos.
+
+app.post('/ex07/', (req, res) => {
+    const array = [req.body.n1, req.body.n2, req.body.n3, req.body.n4, req.body.n5, req.body.n6, req.body.n7, req.body.n8, req.body.n9, req.body.n10, req.body.n11, req.body.n12, req.body.n13, req.body.n14, req.body.n15];
+
+    let maior = Math.max.apply(null, array); //Função para descobrir qual elemento do array é o maior
+
+    const media = (req.body.n1 + req.body.n2 + req.body.n3 + req.body.n4 + req.body.n5 + req.body.n6 + req.body.n7 + req.body.n8 + req.body.n9 + req.body.n10 + req.body.n11 + req.body.n12 + req.body.n13 + req.body.n14 + req.body.n15)/15;
+    
+    res.send(`Dos números enviados o maior deles é: ${maior}.
+    A média dos números enviados é: ${media}.`);
+});
+
+
 
 app.listen(3000, () => {
     console.log(`Api iniciada! Rodando em http://localhost:3000`);
